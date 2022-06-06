a7f4396b7cb9db02e7a31c28b9bfdee45c99fb71
teye <teyezilliqa.com>
2022-06-06 12:58:17 +0800

    add test cases

--- contracts/contracts/Marketplace.sol
+++ contracts/contracts/Marketplace.sol
@@ -1,55 +1,183 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 /**
- * NFT Marketplace contract based on instant sale price selling method
+ * Custom errors
+ */
+error NotApproved();
+error NotListed();
+error NoPaymentToWithdraw();
+error NotTokenOwner();
+error PriceLessThanZero();
+error PriceSameAsPrevious();
+error PriceNotMet(address tokenAddress, uint256 tokenId, uint256 expectedPrice);
+error TokenListed(address tokenAddress, uint256 tokenId);
+
+/**
+ * Self-study NFT Marketplace contract based on instant sale price selling method
+ * Marketplace allows users to sell ERC-721 NFT
  * Referenced from https://blog.chain.link/how-to-build-an-nft-marketplace-with-hardhat-and-solidity/
  */
-contract Marketplace is ReentrancyGuard {
-    // TODO
-    // mapping of 
+contract Marketplace is ReentrancyGuard, Ownable {
 
     struct Listing {
         address tokenAddress;
+        address seller;
         uint tokenId;
         uint price;
     }
 
+    // events
+    event NFTListed();
+    event NFTBought();
+    event ListingUpdated(address indexed _from, address indexed _tokenAddress, uint256 tokenId);
+    event NFTDeListed(address indexed _from, address indexed _tokenAddress, uint256 tokenId);
+
     // state variables
-    // seller -> (sales_id -> Listing)
-    // nft -> (token_id -> (seller, price))
-    mapping(address => mapping(uint256 => Listing)) private salesListings;
+    // seller -> (sales_id -> Listing) ?
+    mapping(address => mapping(uint256 => Listing)) private salesListings;  // nft -> (token_id -> (seller, price))
+    mapping(address => uint256) private salesProceeds;                      // seller -> profits
 
     // modifiers
+    modifier isListed(address tokenAddress, uint256 tokenId) {
+        if (salesListings[tokenAddress][tokenId].price <= 0) {
+            revert NotListed();
+        }
+        _;
+    }
+
+    modifier isNotListed(address tokenAddress, uint256 tokenId) {
+        if (salesListings[tokenAddress][tokenId].price > 0) {
+            revert TokenListed(tokenAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isTokenOwner(address tokenAddress, uint256 tokenId) {
+        IERC721 nft = IERC721(tokenAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (msg.sender != owner) {
+            revert NotTokenOwner();
+        }
+        _;
+    }
+
+    modifier isApproved(address tokenAddress, uint256 tokenId) {
+        IERC721 nft = IERC721(tokenAddress);
+        if (nft.getApproved(tokenId) != address(this)) {
+            revert NotApproved();
+        }
+        _;
+    }
 
     /**
      * @notice list an NFT for sale
      * 
      */
-    function sell() external {
-        // must be token owner
-        // must not be listed
+    function sell(address tokenAddress, uint256 tokenId, uint256 price) external 
+        isNotListed(tokenAddress, tokenId)
+        isTokenOwner(tokenAddress, tokenId)
+        isApproved(tokenAddress, tokenId)
+    {
+        if (price <= 0) {
+            revert PriceLessThanZero();
+        }
+        salesListings[tokenAddress][tokenId] = Listing(tokenAddress, msg.sender, tokenId, price);
+        emit NFTListed();
     }
 
-    function buy() {
-        
+    /**
+     * @notice purchase an NFT on the marketplace
+     */
+    function buy(address tokenAddress, uint256 tokenId) external payable 
+        isListed(tokenAddress, tokenId)
+        isApproved(tokenAddress, tokenId)
+        nonReentrant
+    {
+        Listing memory listedNFT = salesListings[tokenAddress][tokenId];
+        if (msg.value < listedNFT.price) {
+            revert PriceNotMet(tokenAddress, tokenId, listedNFT.price);
+        }
+
+        // don't transfer seller the profits directly; let user withdraw themselves
+        // https://fravoll.github.io/solidity-patterns/pull_over_push.html
+        salesProceeds[listedNFT.seller] += msg.value;
+
+        // delete entry first then transfer NFT to buyer
+        delete salesListings[tokenAddress][tokenId];
+        IERC721(tokenAddress).safeTransferFrom(listedNFT.seller, msg.sender, tokenId);
+        emit NFTBought();
     }
 
-    function updateListing() {
+    /**
+     * @notice update a listing; only token owner can update
+     * @dev    new price must not be equal to old price
+     */
+    function updateListing(address tokenAddress, uint256 tokenId, uint256 newPrice) external 
+        isListed(tokenAddress, tokenId)
+        isTokenOwner(tokenAddress, tokenId)
+        nonReentrant
+    {
+        // price must not be the same as previous
+        if (newPrice <= 0) {
+            revert PriceLessThanZero();
+        }
 
+        Listing memory listedNFT = salesListings[tokenAddress][tokenId];
+        if (newPrice == listedNFT.price) {
+            revert PriceSameAsPrevious();
+        }
+
+        salesListings[tokenAddress][tokenId].price = newPrice;
+        emit ListingUpdated(msg.sender, tokenAddress, tokenId);
     }
 
-    function cancelListing() {
-
+    /**
+     * @notice cancel a listing; only token owner can cancel
+     */
+    function cancelListing(address tokenAddress, uint256 tokenId) external
+        isListed(tokenAddress, tokenId)
+        isTokenOwner(tokenAddress, tokenId)
+    {
+        delete salesListings[tokenAddress][tokenId];
+        emit NFTDeListed(msg.sender, tokenAddress, tokenId);
     }
 
-    function getListing() {
+    /**
+     * @notice used by seller to withdraw sales profits
+     */
+    function withdrawPayment() external {
+        uint256 payment = salesProceeds[msg.sender];
+        if (payment <= 0) {
+            revert NoPaymentToWithdraw();
+        }
 
+        salesProceeds[msg.sender] = 0;
+
+        address payable receiver = payable(msg.sender);
+        receiver.transfer(payment);
     }
 
-    function getListingsByOwner() {
+    /**
+     * @notice get a specific listing details
+     */
+    function getListing(address tokenAddress, uint256 tokenId) external view returns (Listing memory) {
+        return salesListings[tokenAddress][tokenId];
+    }
 
+    // function getListingsByOwner() {
+
+    // }
+
+    /** 
+     * @notice retrieve the sales profits
+     */
+    function getProceeds(address seller) external view returns (uint256) {
+        return salesProceeds[seller];
     }
 }
---
+++ contracts/test/marketplace-test.js
@@ -0,0 +1,188 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Marketplace contract", function () {
+
+    // global test variables
+    let marketplaceFactory;
+    let marketplaceDeployed;
+    let nftFactory;
+    let nftDeployed;
+    let owner;
+    let addr1;
+    let addr2;
+
+    beforeEach(async function() {
+        [owner, addr1, addr2] = await ethers.getSigners();
+        marketplaceFactory = await ethers.getContractFactory("Marketplace");
+        nftFactory = await ethers.getContractFactory("DummyNFT");
+        marketplaceDeployed = await marketplaceFactory.deploy();
+        nftDeployed = await nftFactory.connect(addr1).deploy();
+
+        // mint nft - addr1
+        await nftDeployed.connect(addr1).mint({
+            value: ethers.utils.parseEther("0.001")
+        });
+    });
+
+    describe("Sell", function() {
+        it("Should list NFT for sale", async function() {
+            // approve first
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+            // sell
+            const sellTx = await marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000");
+            await expect(sellTx).to.emit(marketplaceDeployed, 'NFTListed');
+        });
+    
+        it("Should revert if listing an item that is already listed", async function() {
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+    
+            const sellTx = await marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000");
+            await expect(sellTx).to.emit(marketplaceDeployed, 'NFTListed');
+    
+            // sell again
+            await expect(marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000")).to.be.revertedWith('TokenListed');
+        });
+    
+        it("Should revert when listing an item that is not token owner", async function() {
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+            await expect(marketplaceDeployed.connect(addr2).sell(nftDeployed.address, 1, "1000000000000000")).to.be.revertedWith('NotTokenOwner');
+        });
+    
+        it("Should revert when listing an item that is not approved", async function() {
+            await expect(marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000")).to.be.revertedWith('NotApproved');
+        });
+    });
+
+    describe("Buy", function() {
+        beforeEach(async function() {
+            // sell token 1
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+            await marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000");
+        });
+
+        it("Should buy item", async function() {
+            const tx = await marketplaceDeployed.connect(addr2).buy(nftDeployed.address, 1, {
+                value: ethers.utils.parseEther("0.001")
+            })
+
+            await expect(tx).to.emit(marketplaceDeployed, 'NFTBought');
+
+            // check sales listing is deleted
+            const listing = await marketplaceDeployed.getListing(nftDeployed.address, 1);
+            expect(listing.seller).to.equal("0x0000000000000000000000000000000000000000");
+
+            // check sales proceed is updated
+            const sellerProfits = await marketplaceDeployed.getProceeds(addr1.address);
+            expect(sellerProfits).to.equal("1000000000000000");
+
+            // check NFT is transferred
+            const newTokenOwner = await nftDeployed.ownerOf(1);
+            expect(newTokenOwner).to.equal(addr2.address);
+        });
+
+        it("Should revert when buying a non-listed item", async function() {
+            await expect(marketplaceDeployed.connect(addr2).buy(nftDeployed.address, 9999, {
+                value: ethers.utils.parseEther("0.001")
+            })).to.be.revertedWith('NotListed');
+        });
+    
+        it("Should revert when buying a listed item not yet approved", async function() {
+            // seller clear approval
+            await nftDeployed.connect(addr1).approve("0x0000000000000000000000000000000000000000", 1);
+            await expect(marketplaceDeployed.connect(addr2).buy(nftDeployed.address, 1, {
+                value: ethers.utils.parseEther("0.001")
+            })).to.be.revertedWith('NotApproved');
+        });
+    
+        it("Should revert when buying without sufficient payment", async function() {
+            await expect(marketplaceDeployed.connect(addr2).buy(nftDeployed.address, 1)).to.be.revertedWith('PriceNotMet');
+        });
+    });
+
+    describe("Withdraw proceeds", function() {
+        beforeEach(async function() {
+            // sell token 1
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+            await marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000");
+
+            // buy token 1
+            await marketplaceDeployed.connect(addr2).buy(nftDeployed.address, 1, {
+                value: ethers.utils.parseEther("0.001")
+            })
+        });
+
+        it("Should withdraw payment", async function() {
+            const initBalance = await ethers.provider.getBalance(addr1.address);
+            const initProceeds = await marketplaceDeployed.getProceeds(addr1.address);
+            expect(initProceeds).to.equal("1000000000000000");
+
+            await marketplaceDeployed.connect(addr1).withdrawPayment();
+            const newBalance = await ethers.provider.getBalance(addr1.address);
+            const newProceeds = await marketplaceDeployed.getProceeds(addr1.address);
+            // console.log("init: ", initBalance.toString());
+            // console.log("new: ", newBalance.toString());
+            
+            expect(Number(newBalance)).to.be.greaterThan(Number(initBalance));
+            expect(newProceeds).to.equal(0);
+        });
+
+        it("Should revert when no payment to withdraw", async function() {
+            await expect(marketplaceDeployed.connect(addr2).withdrawPayment()).to.be.revertedWith('NoPaymentToWithdraw');
+        });
+    });
+
+    describe("Cancel listing", function() {
+        beforeEach(async function() {
+            // sell token 1
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+            await marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000");
+        });
+
+        it("Should cancel listing", async function() {
+            const tx = await marketplaceDeployed.connect(addr1).cancelListing(nftDeployed.address, 1);
+            await expect(tx).to.emit(marketplaceDeployed, "NFTDeListed").withArgs(addr1.address, nftDeployed.address, 1);
+
+            // check sales listing data
+            const listing = await marketplaceDeployed.getListing(nftDeployed.address, 1);
+            expect(listing.seller).to.equal("0x0000000000000000000000000000000000000000");
+        });
+
+        it("Should revert when cancelling a non listed item", async function() {
+            await expect(marketplaceDeployed.connect(addr1).cancelListing(nftDeployed.address, 2)).to.be.revertedWith("NotListed");
+        });
+
+        it("Should revert when cancelling as not the token owner", async function() {
+            await expect(marketplaceDeployed.connect(addr2).cancelListing(nftDeployed.address, 1)).to.be.revertedWith("NotTokenOwner");
+        });
+    });
+
+    describe("Update listing", function() {
+        beforeEach(async function() {
+            // sell token 1
+            await nftDeployed.connect(addr1).approve(marketplaceDeployed.address, 1);
+            await marketplaceDeployed.connect(addr1).sell(nftDeployed.address, 1, "1000000000000000");
+        });
+
+        it("Should update listing", async function() {
+
+        });
+
+        it("Should revert when updating a non listed item", async function() {
+            await expect(marketplaceDeployed.connect(addr1).updateListing(nftDeployed.address, 9999, "1000000000000000")).to.be.revertedWith("NotListed");
+        });
+
+        it("Should revert when updating as not the token owner", async function() {
+            await expect(marketplaceDeployed.connect(addr2).updateListing(nftDeployed.address, 1, "2000000000000000")).to.be.revertedWith("NotTokenOwner");
+        });
+
+        it("Should revert when updating listing new price is zero", async function() {
+            await expect(marketplaceDeployed.connect(addr1).updateListing(nftDeployed.address, 1, "0")).to.be.revertedWith("PriceLessThanZero");
+        });
+
+        it("Should revert when updating listing new price is identical to previous price", async function() {
+            await expect(marketplaceDeployed.connect(addr1).updateListing(nftDeployed.address, 1, "1000000000000000")).to.be.revertedWith("PriceSameAsPrevious");
+        });
+    });
+
+});
