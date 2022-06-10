333bc41c0854506ba381ddf682652b1ac05fbd3a
teye <teyezilliqa.com>
2022-06-10 14:30:25 +0800

    add buy call

--- frontend/src/components/token-details.tsx
+++ frontend/src/components/token-details.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { ethers } from "ethers";
 import useSWR from "swr";
 import { MARKETPLACE_ABI } from "../abi/marketplaceABI";
+import { useAppSelector } from "../store/hooks";
 
 
 const fetchNFTDetatils = async (
@@ -13,6 +14,7 @@ const fetchNFTDetatils = async (
     let tokenAddress = '';
     let tokenId = '';
     let seller = '';
+    let price = '';
 
     const assetQueryArray = assetQuery?.split(":") ?? [];
 
@@ -28,6 +30,7 @@ const fetchNFTDetatils = async (
             if (listing && listing.tokenAddress !== "0x0000000000000000000000000000000000000000") {
                 tokenAddress = listing.tokenAddress;
                 tokenId = `${listing.tokenId}`;
+                price = `${listing.price}`;
                 seller = listing.seller;
             }
         } catch (e) {
@@ -38,23 +41,72 @@ const fetchNFTDetatils = async (
     return {
         tokenAddress,
         tokenId,
-        seller
+        seller,
+        price
     }
 }
+
+let provider: any;
 
 function TokenDetails() {
     let marketplaceAddress = process.env.REACT_APP_MARKETPLACE_CONTRACT ?? "a";
     let navigate = useNavigate();
     const { assetQuery } = useParams();
     const { data, error } = useSWR([`swr_fetch_nft_details`, marketplaceAddress, assetQuery], fetchNFTDetatils);
-    const [tokenAddress, setTokenAddress] = useState('');
-    const [tokenId, setTokenId] = useState('');
+    
+    const userState = useAppSelector((state) => state.user);
 
     useEffect(() => {
         if (data && (!data?.tokenAddress || !data.tokenId)) {
             navigate("/error", { replace: true })
         }
     }, [data, navigate]);
+
+    /**
+     * buy an NFT
+     * @param tokenAddress 
+     * @param tokenId 
+     * @param price Wei 
+     * @returns 
+     */
+    const onBuy = async (
+        tokenAddress: string, 
+        tokenId: string, 
+        price: string
+    ) => {
+        if (!userState.isConnected) {
+            console.error("Please connect wallet to continue.");
+            return;
+        }
+
+        console.log(price);
+
+        // TODO: check user balance
+        provider = ethers.getDefaultProvider("http://localhost:8545");
+
+        const userBal = await provider.getBalance(userState.wallet);
+        if (ethers.BigNumber.from(userBal).lt(ethers.BigNumber.from(price))) {
+            console.error(`Insufficient balance; Require ${ethers.utils.formatEther(price)} ETH; Please top up wallet.`);
+            return;
+        }
+
+        const signer = provider.getSigner();
+
+        const deployed = new ethers.Contract(marketplaceAddress, MARKETPLACE_ABI, signer);
+
+        try {
+            const tx = await deployed.buy(
+                tokenAddress, 
+                tokenId,
+                {
+                    value: price
+                }
+            );
+            console.log("tx: ", tx.hash);
+        } catch (e) {
+            console.error(e);
+        }
+    }
     
     return (
         <div className="container mx-auto">
@@ -66,10 +118,20 @@ function TokenDetails() {
                     {/* nft image */}
                     <div className="rounded-xl bg-slate-800 h-[400px] w-[400px]"></div>
                     {/* nft details */}
-                    <div className="bg-blue-400">
+                    <div className="p-4">
                         <h1>Token Name #{data.tokenId}</h1>
-                        <div>Owned by {data.seller.toLowerCase()}</div>
-                        {/* add Buy button */}
+                        <div className="text-gray-500 text-[0.9em]">
+                            Owned by <span className="text-blue-500">{data.seller.toLowerCase()}</span>
+                        </div>
+                        <div className="bg-white border border-gray-200 rounded-md p-4 bg-slate-50 my-8">
+                            <h3 className="font-semibold text-zinc-400 text-[0.9em]">Current price</h3>
+                            <div className="font-bold text-3xl text-neutral-800">{ethers.utils.formatEther(data.price)} ETH</div>
+                        </div>
+                        <button 
+                            className="bg-black font-bold text-sm text-white py-2 px-4 rounded mr-4"
+                            onClick={() => onBuy(data.tokenAddress, data.tokenId, data.price)}>
+                            Buy Now
+                        </button>
                     </div>
                 </div>
             }
