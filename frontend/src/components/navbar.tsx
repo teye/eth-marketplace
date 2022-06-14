709b4d8ed67bbec0926204570f08852e17237476
teye <teyezilliqa.com>
2022-06-14 15:28:03 +0800

    add user icon

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 import { ethers } from "ethers";
 import { USER_RESET, UPDATE_IS_CONNECTED, UPDATE_WALLET } from "../store/userSlice";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { User } from "../icons/user";
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment } from "react";
 
 
 let provider;
@@ -69,7 +72,89 @@ function Navbar() {
                 {
                     userState.wallet &&
                     <li className="nav-item font-kanit text-sm lowercase">
-                        <div>{userState.wallet}</div>
+                        <Menu as="div" className="relative inline-block text-left">
+                            <div>
+                            <Menu.Button className="inline-flex w-full justify-center rounded-full border-2 border-black shadow-sm text-sm font-medium p-1 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                                <User 
+                                    width="26"
+                                    height="26"
+                                />
+                            </Menu.Button>
+                            </div>
+                            <Transition
+                                as={Fragment}
+                                enter="transition ease-out duration-100"
+                                enterFrom="transform opacity-0 scale-95"
+                                enterTo="transform opacity-100 scale-100"
+                                leave="transition ease-in duration-75"
+                                leaveFrom="transform opacity-100 scale-100"
+                                leaveTo="transform opacity-0 scale-95"
+                            >
+                            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <div className="px-1 py-1 ">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                    <button
+                                        className={`${
+                                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        Edit
+                                    </button>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                    <button
+                                        className={`${
+                                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        Duplicate
+                                    </button>
+                                    )}
+                                </Menu.Item>
+                                </div>
+                                <div className="px-1 py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                    <button
+                                        className={`${
+                                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        Archive
+                                    </button>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                    <button
+                                        className={`${
+                                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        Move
+                                    </button>
+                                    )}
+                                </Menu.Item>
+                                </div>
+                                <div className="px-1 py-1">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                    <button
+                                        className={`${
+                                        active ? 'bg-violet-500 text-white' : 'text-gray-900'
+                                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    >
+                                        Delete
+                                    </button>
+                                    )}
+                                </Menu.Item>
+                                </div>
+                            </Menu.Items>
+                            </Transition>
+                        </Menu>
                     </li>
                 }
             </ul>
