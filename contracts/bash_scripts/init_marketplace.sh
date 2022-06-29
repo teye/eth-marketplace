e872a58d676840f4a96c41efadfdad82272f81d4
teye <teyezilliqa.com>
2022-06-29 14:35:15 +0800

    edit bash script

--- contracts/bash_scripts/init_marketplace.sh
+++ contracts/bash_scripts/init_marketplace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running init marketplace script..."
-npx hardhat --network localhost run ../scripts/deploy-basic-nft.js
+# npx hardhat --network localhost run ../scripts/deploy-basic-nft.js
 npx hardhat --network localhost run ../scripts/deploy-marketplace.js
-npx hardhat --network localhost run ../scripts/mint-and-list-nft.js
+# npx hardhat --network localhost run ../scripts/mint-and-list-nft.js
 
